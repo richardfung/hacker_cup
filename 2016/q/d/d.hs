ac06f050e6facee23c45b8664c0126bd5877b16e
Richard Fung <minesasecret@gmail.com>
2016-01-10 23:18:08 -0800

    2016/q/d WIP

--- 2016/q/d/d.hs
+++ 2016/q/d/d.hs
@@ -1,16 +1,31 @@
 import Control.Monad.ST
+import Data.Array
 import Data.Array.ST
 import Data.STRef
 
-data STNode s = STNode (STArray s Char (STNode s)) (STRef s Bool) | Empty
+data Node = Node (Array Char Node) Bool | Empty
+data STNode s = STNode (STArray s Char (STNode s)) (STRef s Bool) | STEmpty
 
 main = do
     print "Why is this so hard?"
 
 genSTNode :: String -> STNode s -> ST s ()
 genSTNode [] _ = return ()
-{-
-genSTNode [c] Empty 
-genSTNode (c:cs) node = do
-    
-    -}
+genSTNode (c:[]) (STNode stArray _) = do
+    child <- readArray stArray c
+    case child of STEmpty -> do nextBool <- newSTRef True
+                                nextArray <- newArray ('a', 'z') STEmpty
+                                writeArray stArray c $ (STNode nextArray nextBool)
+                  STNode childArray stBool -> writeSTRef stBool True
+genSTNode (c:cs) (STNode stArray _) = do
+    child <- readArray stArray c
+    case child of STEmpty -> do nextBool <- newSTRef False
+                                nextArray <- newArray ('a', 'z') STEmpty
+                                writeArray stArray c $ (STNode nextArray nextBool)
+                                child <- readArray stArray c
+                                genSTNode cs child
+                  otherwise -> genSTNode cs child
+
+stNodeToNode :: STNode s -> Node
+stNodeToNode STEmpty = Empty
+stNodeToNode (STNode stArray stBool) = 

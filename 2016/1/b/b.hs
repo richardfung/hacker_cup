0a3d76557be4eab32337d6c7d33462daf76bfc0c
Richard Fung <richard@extrahop.com>
2016-01-22 15:03:28 -0800

    2016/1/b solved

--- 2016/1/b/b.hs
+++ 2016/1/b/b.hs
@@ -1,5 +1,6 @@
 import Control.Applicative ((<$>))
 import Control.Monad (forM_)
+import Data.Map as M
 import Data.Set as S
 import Debug.Trace
 import Text.Printf
@@ -15,18 +16,21 @@ solve :: Int -> [Int] -> Int -> Int -> Int
 solve l ws m d =
     let washSet = Prelude.foldr S.insert S.empty
                   $ Prelude.map (\(a,id') -> (a,a,id')) $ zip ws [1..]
-        drySet = Prelude.foldr S.insert S.empty $ zip (repeat 0) [1..m]
-    in dryTime (washTimes l washSet []) drySet d
+        dryMap = M.singleton 0 m
+    in dryTime (washTimes l washSet []) dryMap d
 
-dryTime :: [Int] -> S.Set (Int, Int) -> Int -> Int
-dryTime [] ds _ = fst $ S.findMax ds
-dryTime (t:ts) ds d =
-    let (tStart, id') = findMin ds
-    in dryTime ts (insert (max t tStart + d, id') $ delete (tStart, id') ds) d
+dryTime :: [Int] -> M.Map Int Int -> Int -> Int
+dryTime [] dm _ = fst $ M.findMax dm
+dryTime (t:ts) !dm d =
+    let (tStart, count) = M.findMin dm
+        k = max t tStart + d
+        newDm = M.insert k (findWithDefault 0 k dm + 1)
+            $ if count > 1 then M.insert tStart (count-1) dm else M.delete tStart dm
+    in dryTime ts newDm d
 
 washTimes :: Int -> S.Set (Int, Int, Int) -> [Int] -> [Int]
 washTimes 0 ws acc = reverse acc
-washTimes loads ws acc =
+washTimes loads !ws acc =
     let (a,b,id') = S.findMin ws
     in washTimes (loads-1) (S.insert (a+b, b, id') $ S.delete (a, b, id') ws)
                  (a:acc)
